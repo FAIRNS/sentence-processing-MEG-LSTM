038e58287d12ccd3086bdab284d08bf1e97fa533
German Kruszewski <germank@fb.com>
2018-03-12 16:48:21 +0100

    computing correlations between units and various types of depths

--- Code/LSTM/model-analysis/extract-activations.py
+++ Code/LSTM/model-analysis/extract-activations.py
@@ -8,6 +8,8 @@ import json
 sys.path.append(os.path.abspath('../src/word_language_model'))
 import data
 import numpy as np
+import h5py
+import pickle
 
 parser = argparse.ArgumentParser(
     description='PyTorch PennTreeBank RNN/LSTM Language Model')
@@ -18,6 +20,10 @@ parser.add_argument('-i', '--input', required=True,
 parser.add_argument('-v', '--vocabulary', default='reduced_vocab.txt')
 parser.add_argument('-o', '--output', help='Destination for the output vectors')
 parser.add_argument('--perplexity', action='store_true', default=False)
+parser.add_argument('--eos-separator', default='</s>')
+parser.add_argument('--fixed-length-arrays', action='store_true', default=False,
+        help='Save the result to a single fixed-length array')
+parser.add_argument('--format', default='npz', choices=['npz', 'hdf5', 'pkl'])
 
 
 
@@ -30,59 +36,85 @@ args = parser.parse_args()
 #extra_args.__override_args__ = args
 
 
-sentences = [l.rstrip('\n').split(" ") for l in open(args.input)]
+vocab = data.Dictionary(args.vocabulary)
+sentences = []
+for l in open(args.input):
+    sentence = l.rstrip('\n').split(" ") 
+    sentences.append(sentence)
+sentences = np.array(sentences)
+
+
+print('Loading models...')
+sentence_length = [len(s) for s in sentences]
+max_length = max(*sentence_length)
 import lstm
 model = torch.load(args.model)
 model.rnn.flatten_parameters()
 # hack the forward function to send an extra argument containing the model parameters
 model.rnn.forward = lambda input, hidden: lstm.forward(model.rnn, input, hidden)
 
-vocab = data.Dictionary(args.vocabulary)
 
 
 # output buffers
-vectors = np.zeros((len(sentences), 2 *model.nhid*model.nlayers, 8))
-log_probabilities = np.zeros((len(sentences), 8))
-gates = {k: np.zeros((len(sentences), model.nhid*model.nlayers, 8)) for k in ['in', 'forget', 'out', 'c_tilde']}
+if args.fixed_length_arrays:
+    vectors = np.zeros((len(sentences), 2 *model.nhid*model.nlayers, max_length))
+    log_probabilities =  np.zeros((len(sentences), max_length))
+    gates = {k: np.zeros((len(sentences), model.nhid*model.nlayers, max_length)) for k in ['in', 'forget', 'out', 'c_tilde']}
+else:
+    vectors = [np.zeros((2*model.nhid*model.nlayers, len(s))) for s in sentences] #np.zeros((len(sentences), 2 *model.nhid*model.nlayers, max_length))
+    log_probabilities = [np.zeros(len(s)) for s in sentences] # np.zeros((len(sentences), max_length))
+    gates = {k: [np.zeros((model.nhid*model.nlayers, len(s))) for s in sentences] for k in ['in', 'forget', 'out', 'c_tilde']} #np.zeros((len(sentences), model.nhid*model.nlayers, max_length)) for k in ['in', 'forget', 'out', 'c_tilde']}
 
 for i, s in enumerate(sentences):
+    sys.stdout.write("{}% complete ({} / {})\r".format(int(i/len(sentences) * 100), i, len(sentences)))
     out = None
     # reinit hidden
     hidden = model.init_hidden(1) 
     # intitialize with end of sentence
-    inp = torch.autograd.Variable(torch.LongTensor([[vocab.word2idx['</s>']]]).cuda())
+    inp = torch.autograd.Variable(torch.LongTensor([[vocab.word2idx[args.eos_separator]]]).cuda())
     out, hidden = model(inp, hidden)
     out = torch.nn.functional.log_softmax(out[0]).unsqueeze(0)
     for j, w in enumerate(s):
         # store the surprisal for the current word
-        log_probabilities[i,j] = out[0,0,vocab.word2idx[w]].data[0]
+        log_probabilities[i][j] = out[0,0,vocab.word2idx[w]].data[0]
 
         inp = torch.autograd.Variable(torch.LongTensor([[vocab.word2idx[w]]]).cuda())
         out, hidden = model(inp, hidden)
         out = torch.nn.functional.log_softmax(out[0]).unsqueeze(0)
 
-        vectors[i,:,j] = torch.cat([h.data.view(1,1,-1) for h in hidden],2).cpu().numpy()
+        vectors[i][:,j] = torch.cat([h.data.view(1,1,-1) for h in hidden],2).cpu().numpy()
         # we can retrieve the gates thanks to the hacked function
         for k, gates_k in gates.items():
-            gates_k[i,:,j] = torch.cat([g[k].data for g in model.rnn.last_gates],1).cpu().numpy()
+            gates_k[i][:,j] = torch.cat([g[k].data for g in model.rnn.last_gates],1).cpu().numpy()
 
 if args.perplexity:
     print ("{}".format(log_probabilities.shape))
     print ("{}".format(
-            math.exp(-log_probabilities.sum()/log_probabilities.size)))
+            math.exp(-log_probabilities.sum()/(log_probabilities!=0).sum())))
 else:
-    print ("DONE. Perplexity: {}".format(
-            math.exp(-log_probabilities.sum()/log_probabilities.size)))
+    #print ("DONE. Perplexity: {}".format(
+    #        math.exp(-log_probabilities.sum()/((log_probabilities!=0).sum()))))
 
     out = {
-        'sentences': sentences,
         'vectors': vectors,
         'log_probabilities': log_probabilities
         }
+    if args.format != 'hdf5':
+        out['sentences'] = sentences
+
     for k, g in gates.items():
         out['gates.{}'.format(k)] = g
+    out['sentence_length'] = np.array(sentence_length)
 
-    np.savez(args.output, **out)
+    if args.format == 'npz':
+        np.savez(args.output, **out)
+    elif args.format == 'hdf5':
+        with h5py.File("{}.h5".format(args.output), "w") as hf:
+            for k,v in out.items():
+                dset = hf.create_dataset(k, data=v)
+    elif args.format == 'pkl':
+        with open(args.output + '.pkl', 'wb') as fout:
+            pickle.dump(out, fout, -1)
 
 
 
--- Code/LSTM/model-analysis/plot-activations.py
+++ Code/LSTM/model-analysis/plot-activations.py
@@ -9,52 +9,105 @@ import matplotlib.pyplot as plt
 import sys
 import os
 import scipy.stats
+import random
 import operator 
+import pickle
 
 from collections import defaultdict
 
 
 def main():
-    ap = argparse.ArgumentParser()
+    ap = argparse.ArgumentParser(description="""
+    Takes the network activations for a set of sentences and a reference gold
+    standard description of these sentences and produces plots relating 
+    individual units to the data in the gold standard.
+    """)
     ap.add_argument('vectors')
-    ap.add_argument('info')
-    ap.add_argument('--group', default='structure')
+    ap.add_argument('reference')
+    ap.add_argument('-t', '--analysis-type', required=True, choices=[
+        'categories', 'correlation'])
+    ap.add_argument('-g', '--group', default='structure', help=
+            'Field of the gold-standard reference file to be used in plots')
     ap.add_argument('-o', '--output', required=True, help='Directory where to '
             'output the figures')
 
     args = ap.parse_args()
 
     print('Loading data...')
-    vectors = np.load(args.vectors)
-    nhid = 1000
+    if args.vectors.endswith('npz'):
+        vectors = np.load(args.vectors, 'r')
+    elif args.vectors.endswith('pkl'):
+        vectors = pickle.load(open(args.vectors, 'rb'))
+    nhid = int(vectors['vectors'][0].shape[0] / 2)
     plot_vectors = {}
     for name in ['gates.in', 'gates.out', 'gates.forget', 'gates.c_tilde']:
         plot_vectors[name] = vectors[name]
-    plot_vectors['hidden'] = vectors['vectors'][:,:nhid,:]
-    plot_vectors['cell'] = vectors['vectors'][:,nhid:,:]
+    if isinstance(vectors['vectors'], list):
+        plot_vectors['hidden'] = [v[:nhid, :] for v in vectors['vectors']]
+        plot_vectors['cell'] = [v[nhid:, :] for v in vectors['vectors']]
+    else:
+        plot_vectors['hidden'] = vectors['vectors'][:,:nhid,:]
+        plot_vectors['cell'] = vectors['vectors'][:,nhid:,:]
 
-    info = pickle.load(open(args.info, 'rb'))
-    # get the indexes of the trials for each class
-    class_indexes = defaultdict(list)
-    for i,t in enumerate(info):
-        class_indexes[t[args.group]].append(i)
+    reference = pickle.load(open(args.reference, 'rb'))
 
-    group_labels = {'structure':  {2: "2-6", 1: "4-4", 3: "6-2"}}
+    if args.analysis_type ==  'categories':
+        class_indexes = defaultdict(list)
+        # get the indexes of the trials for each class
+        for i,t in enumerate(reference):
+            class_indexes[t[args.group]].append(i)
+
+        group_labels = {'structure':  {2: "2-6", 1: "4-4", 3: "6-2"}}
+    elif args.analysis_type == 'correlation':
+        series = []
+        plot_vectors_series = defaultdict(list)
+        for i,t in enumerate(reference):
+            # remove void entries, the first one (always the same) and the last one (change of sentence confound)
+            content = np.array([[j,x] for j,x in enumerate(t[args.group][1:-1]) if x != "-"])
+            assert len(vectors['sentences'][i]) == len(next(iter(t.values())))
+            # we only use sentences of a given max length and having
+            # a series with a minimum length, and there must be SOME variability
+            if len(t[args.group]) < 60 and len(t[args.group]) > 10 and content.shape[0] > 1 and not (content[:,1] == content[0,1]).all():
+                valid_idxs = content[:,0].astype(np.int)
+                series.append(content[:,1])
+                for k, v in plot_vectors.items():
+                    plot_vectors_series[k].append(v[i][:,valid_idxs])
+        #series = np.concatenate(series)
+        # for k,v in plot_vectors_series.items():
+        #     plot_vectors_series[k] = np.concatenate(v, 1)
 
     os.makedirs(args.output, exist_ok=True)
+    print('Computting statistics...')
+    mean_correlations = {}
+    for i,(name, activations) in enumerate(plot_vectors_series.items()):
+        print(name, len(activations), activations[0].shape)
+        mean_correlations[name] = get_mean_correlation(activations, series)
+    statistic = get_max_correlation(mean_correlations)
+    sorted_statistics = (-np.abs(statistic[0])).argsort()
+    with open('{}/statistic.txt'.format(args.output), 'w') as f:
+        for i in sorted_statistics:
+            f.write("{}\t{:.2f} +- {:.2f}\t{:.2f}\n".format(i,statistic[0][i], statistic[1][i], statistic[-1][i]))
+    # get the units with the largest r^2
+    units = sorted_statistics[:25]
     print('Plotting...')
-    pvalues = {}
-    for u in range(nhid):
-        sys.stdout.write('{}\r'.format(u))
-        pvalues[u] = plot({k: v[:,u,:] for k,v in plot_vectors.items()}, class_indexes, 
-                group_labels[args.group], os.path.join(args.output, '{}.svg'.format(u)))
+    for u in units:
+        sys.stdout.write('{:d}\n'.format(u))
+        if args.analysis_type == 'categories':
+            statistic[u] = plot_categorical({k: v[:,u,:] for k,v in plot_vectors.items()}, class_indexes, 
+                    group_labels[args.group], os.path.join(args.output, '{}.svg'.format(u)))
+        elif args.analysis_type == 'correlation':
+            plot_correlation({k: [v2[u, :] for v2 in v] for k,v in plot_vectors_series.items()}, series, 
+                    {k: (v[0][u],v[1][u],v[2][u]) for k, v in mean_correlations.items()},
+                    os.path.join(args.output, '{}.png'.format(u)))
 
-    with open('{}/pvalues.txt'.format(args.output), 'w') as f:
-        for x,p in sorted(pvalues.items(), key=operator.itemgetter(1)):
-            f.write("{}\t{}\n".format(x,p))
     print()
 
-def plot(unit_activations,  class_indexes, class_labels, output):
+
+def is_series_acceptable(xs):
+    return len([x for x in xs if x!="-"]) > 1
+
+
+def plot_categorical(unit_activations, class_indexes, class_labels, output):
     plt.figure(figsize=(12,8))
     min_pvalue = 1
     for i,(name, activations) in enumerate(unit_activations.items()):
@@ -76,5 +129,97 @@ def plot(unit_activations,  class_indexes, class_labels, output):
     plt.clf()
     plt.close()
     return min_pvalue
+
+def get_max_correlation(mean_correlations):
+    '''
+    takes a dictionary of names -> statistics and returns
+    the statistics with the maximum value across names
+    '''
+    name_max = []
+    for j in range(next(iter(mean_correlations.values()))[0].shape[0]):
+        max_rho = None
+        max_name = None
+        for i,(name, correlations) in enumerate(mean_correlations.items()):
+            rho = np.abs(correlations[0][j])
+            if max_rho is None or rho >  max_rho:
+                max_rho = rho
+                max_name = name
+        name_max.append(max_name)
+    max_vals = []
+    for j,name in enumerate(name_max):
+        max_vals.append([s[j] for s in mean_correlations[name]])
+    t_max_vals = tuple(np.array(s) for s in zip(*max_vals))
+    return t_max_vals
+
+def vcorrcoef(X,y):
+    Xm = np.reshape(np.mean(X,axis=1),(X.shape[0],1))
+    ym = np.mean(y)
+    r_num = np.sum((X-Xm)*(y-ym),axis=1)
+    r_den = np.sqrt(np.sum((X-Xm)**2,axis=1)*np.sum((y-ym)**2))
+    r = r_num/r_den
+    return r
+
+def get_mean_correlation(activations, target_series):
+    rs = []
+    print("Computing mean correlation..")
+    idxs = list(range(len(activations)))
+    random.shuffle(idxs)
+    idxs = idxs[:1000]
+    approach = 1
+
+    # just concatenating pearson approach
+    pearsonr = vcorrcoef(np.concatenate(activations, axis=1), np.concatenate(target_series, axis=0))
+    if approach == 1:
+        stat = pearsonr
+    # mean of spearmanr correlations
+    elif approach == 2:
+        for k,i in enumerate(idxs):
+            sys.stdout.write("Sentence {} / {} ({:.0f}% complete)\r".format(k, len(idxs), k/len(idxs)*100))
+            r = np.zeros(activations[i].shape[0]) 
+            for j in range(r.shape[0]):
+                r[j], p = scipy.stats.spearmanr(activations[i][j], target_series[i])
+            rs.append(r)
+        stat = np.mean(rs, axis=0)
+    # mean of pearson correlations
+    elif approach ==3:
+        for k,i in enumerate(idxs):
+            sys.stdout.write("Sentence {} / {} ({:.0f}% complete)\r".format(k, len(idxs), k/len(idxs)*100))
+            r = vcorrcoef(activations[i], np.array([target_series[i]]))
+            rs.append(r)
+        stat = np.mean(rs, axis=0)
+
+    return np.mean(rs, axis=0), np.std(rs, axis=0), pearsonr
+    #return np.mean(rs, axis=0), np.std(rs, axis=0), rs, pearsonr
+
+def plot_correlation(unit_activations, target_series, correlations, output):
+    plt.figure(figsize=(12,8))
+    max_r = 0
+    for i,(name, activations) in enumerate(unit_activations.items()):
+        plt.subplot(231+i)
+        plt.title(name)
+        #scatter here
+        x = np.concatenate(target_series, 0)
+        y = np.concatenate(activations, 0)
+        #plt.scatter(x, y, alpha=0.01)
+        plt.hist2d(x, y, bins=30, cmap=plt.cm.YlOrRd, norm=matplotlib.colors.LogNorm())
+        #plt.hist2d(activations, target_series, bins=400, normed=matplotlib.colors.LogNorm)
+        rho,rho_std,old_r = correlations[name] #get_mean_correlation([np.array([a]) for a in activations], target_series)
+        # fit linear regession
+        z = np.polyfit(x, y, 1)
+        p = np.poly1d(z)
+        xm = x.mean()
+        xs = x.std()
+        x_fit = np.linspace(x.min(), xm+2*xs)
+        y_fit = p(x_fit)
+        x2 = np.linspace(x.min(), x.max())
+        # plot line of best fit
+        plt.plot(x2,p(x2),'k-',label='$\\bar{{\\rho}}={:.2f} \\pm {:.2f}$ ($r={:.2f}$)'.format(rho, rho_std, old_r))
+        plt.legend()
+    plt.savefig(output)
+    plt.clf()
+    plt.close()
+    return max_r
+
+
 main()
 
