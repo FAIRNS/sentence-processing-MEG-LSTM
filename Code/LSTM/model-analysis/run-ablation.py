3e7acc0a7ad78ef52553c05c18f9f7efb24fcb55
German Kruszewski <germank@fb.com>
2018-03-28 13:57:02 +0200

    ablation scripts and ongoing work on controlled correlation

---
+++ Code/LSTM/model-analysis/run-ablation.py
@@ -0,0 +1,18 @@
+# Paths
+script = "$HOME/projects/neurospin/sentence-processing-MEG-LSTM/Code/LSTM/model-analysis/ablation-experiment.py"
+model = "/checkpoint/germank/neurospin/data/trained-english-models/hidden650_batch128_dropout0.2_lr20.0.pt"
+agreement_data = "$HOME/projects/neurospin/sentence-processing-MEG-LSTM/Data/agreement-data/best_model.tab"
+vocab = "$HOME/projects/neurospin/data/trained-english-models/vocab.txt"
+output = "$HOME/projects/neurospin/sentence-processing-MEG-LSTM/Output/ablation-results-2/ablation_results_killing_unit_"
+eos = '"<eos>"'
+format = 'pkl'
+unit_to_kill_from = 1300
+unit_to_kill_to = 1
+
+for seed in range(20):
+    g = 10 # group size of units to kill in including random ones
+
+    cmd = "$HOME/.conda/envs/localconda/bin/python " + script + ' ' + model + ' --input ' + agreement_data + ' --vocabulary ' + vocab + ' --output ' + output +\
+                        ' --eos-separator ' + eos + ' --format ' + format + ' -uf ' + str(unit_to_kill_from) + ' -ut ' + str(unit_to_kill_to) + ' -g '\
+          + str(g) + ' -s ' + str(seed) + ' --cuda'
+    print(cmd)
