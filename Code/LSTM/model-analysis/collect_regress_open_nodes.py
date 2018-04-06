3ce640fa5c72b22d236d1dd010cc6f119be894d4
FAIRNS <yair.lakretz@cea.fr>
2018-04-06 17:07:23 +0200

    Regression number of open nodes

--- Code/LSTM/model-analysis/collect_regress_open_nodes.py
+++ Code/LSTM/model-analysis/collect_regress_open_nodes.py
@@ -19,22 +19,32 @@ params = lsp.params()
 preferences = lsp.preferences()
 
 # ----- Load LASSO model -----
-pkl_filename = 'Regression_number_of_open_nodes_' + settings.y_label + '_MODEL_' + settings.LSTM_pretrained_model + '_h_or_c_' + str(settings.h_or_c) + '.pckl'
-with open(op.join(settings.path2output, pkl_filename), "rb") as f:
-    models = pickle.load(f)
+best_weights = []
+for seed in range(1,6,1):
+    pkl_filename = 'Regression_number_of_open_nodes_' + settings.y_label + '_MODEL_' + settings.LSTM_pretrained_model + '_h_or_c_' + str(settings.h_or_c) + '_seed_'+ str(seed) + '.pckl'
+    with open(op.join(settings.path2output, pkl_filename), "rb") as f:
+        models = pickle.load(f)
 
-# ---- Generate figures --------
+    # ---- Generate figures --------
+    # Regularization path
+    file_name = 'Lasso_coef_and_R_squared_vs_regularization_size.png'
+    plt = pr.regularization_path(models['model_lasso'], settings, params)
+    plt.savefig(op.join(settings.path2figures, file_name))
+    plt.close()
 
-# Regularization path
-file_name = 'Lasso_coef_and_R_squared_vs_regularization_size.png'
-plt = pr.regularization_path(models['model_lasso'], settings, params)
-plt.savefig(op.join(settings.path2figures, file_name))
-plt.close()
+    # Best weights (correspond to optimal regularization size)
+    best_index = models['model_lasso'].best_index_
+    best_weights.append(models['model_lasso'].coefs[best_index, :])
 
-# Best weights (correspond to optimal regularization size)
-best_index = models['model_lasso'].best_index_
-best_weights = models['model_lasso'].coefs[best_index, :]
-file_name = 'Lasso_best_coef.png'
+best_weights = np.vstack(best_weights) # stack back to ndarray
+file_name = 'Lasso_best_coef_' + settings.y_label + '_MODEL_' + settings.LSTM_pretrained_model + '_h_or_c_' + str(settings.h_or_c) + '.png'
 plt = pr.plot_weights(best_weights, settings, params)
 plt.savefig(op.join(settings.path2figures, file_name))
 
+
+# Sort and save to file
+file_name = 'Lasso_best_coef_' + settings.y_label + '_MODEL_' + settings.LSTM_pretrained_model + '_h_or_c_' + str(settings.h_or_c) + '.txt'
+best_weights = np.vstack((np.mean(best_weights, axis=0), range(1,best_weights.shape[1]+1, 1)))
+IX = np.argsort(best_weights[0, :])[::-1]
+best_weights_sorted = np.transpose(best_weights)[IX]
+best_weights_sorted.tofile(op.join(settings.path2output, file_name), sep="\t\n")
