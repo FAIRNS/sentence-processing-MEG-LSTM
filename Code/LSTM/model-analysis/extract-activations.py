974ce0cde235f341940bd7d04d8f5ef54aac4fd5
German Kruszewski <germank@fb.com>
2018-04-16 14:58:32 +0200

    fix extract activations and perplexity

--- Code/LSTM/model-analysis/extract-activations.py
+++ Code/LSTM/model-analysis/extract-activations.py
@@ -5,7 +5,8 @@ import os
 import torch
 import argparse
 import json
-sys.path.append(os.path.abspath('../src/word_language_model'))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+    '../src/word_language_model')))
 import data
 import numpy as np
 import h5py
@@ -25,13 +26,18 @@ parser.add_argument('--eos-separator', default='</s>')
 parser.add_argument('--fixed-length-arrays', action='store_true', default=False,
         help='Save the result to a single fixed-length array')
 parser.add_argument('--cuda', action='store_true', default=False)
-parser.add_argument('--format', default='npz', choices=['npz', 'hdf5', 'pkl'])
+parser.add_argument('--format', default='pkl', choices=['npz', 'hdf5', 'pkl'])
 parser.add_argument('-g', '--get-representations', choices=['lstm', 'word'],
-        action='append', required=True)
+        action='append', default=[])
+parser.add_argument('--use-unk', action='store_true', default=False)
 
 
 args = parser.parse_args()
 
+if args.perplexity and 'lstm' not in args.get_representations:
+    args.get_representations.append('lstm')
+elif not args.perplexity and len(args.get_representations) == 0:
+    raise RuntimeError("Please specify at least one of -g lstm or -g word")
 
 #with open(args.meta, 'r') as f:
 #    meta = json.load(f)
@@ -48,7 +54,7 @@ for l in open(args.input):
 sentences = np.array(sentences)
 
 
-print('Loading models...')
+print('Loading models...', file=sys.stderr)
 sentence_length = [len(s) for s in sentences]
 max_length = max(*sentence_length)
 import lstm
@@ -58,34 +64,39 @@ model = torch.load(args.model)
 model.rnn.forward = lambda input, hidden: lstm.forward(model.rnn, input, hidden)
 
 
-out = {}
+saved = {}
 
 if 'word' in args.get_representations:
-    print('Extracting bow representations')
+    print('Extracting bow representations', file=sys.stderr)
     bow_vectors = np.zeros((len(sentences), model.encoder.embedding_dim, max_length))
     word_vectors = np.zeros((len(sentences), model.encoder.embedding_dim, max_length))
     for i, s in enumerate(tqdm(sentences)):
         bow_h = np.zeros(model.encoder.embedding_dim)
         for j, w in enumerate(s):
-            inp = torch.autograd.Variable(torch.LongTensor([[vocab.word2idx[w]]]))
+            if w not in vocab.word2idx and args.use_unk:
+                w = '<unk>'
+                inp = torch.autograd.Variable(torch.LongTensor([[vocab.word2idx[w]]]))
+
             if args.cuda:
                 inp = inp.cuda()
             w_vec = model.encoder.forward(inp).view(-1).data.cpu().numpy()
             word_vectors[i,:,j] = w_vec
             bow_h += w_vec
             bow_vectors[i,:,j] = bow_h / (j+1)
-    out['word_vectors'] = word_vectors
-    out['bow_vectors'] = bow_vectors
+    saved['word_vectors'] = word_vectors
+    saved['bow_vectors'] = bow_vectors
 
 if 'lstm' in args.get_representations:
-    print('Extracting LSTM representations')
+    print('Extracting LSTM representations', file=sys.stderr)
     # output buffers
     if args.fixed_length_arrays:
         log_probabilities =  np.zeros((len(sentences), max_length))
-        vectors = {k: np.zeros((len(sentences), model.nhid*model.nlayers, max_length)) for k in ['in', 'forget', 'out', 'c_tilde', 'hidden', 'cell']}
+        if not args.perplexity:
+            vectors = {k: np.zeros((len(sentences), model.nhid*model.nlayers, max_length)) for k in ['gates.in', 'gates.forget', 'gates.out', 'gates.c_tilde', 'hidden', 'cell']}
     else:
         log_probabilities = [np.zeros(len(s)) for s in tqdm(sentences)] # np.zeros((len(sentences), max_length))
-        vectors = {k: [np.zeros((model.nhid*model.nlayers, len(s))) for s in tqdm(sentences)] for k in ['in', 'forget', 'out', 'c_tilde', 'hidden', 'cell']} #np.zeros((len(sentences), model.nhid*model.nlayers, max_length)) for k in ['in', 'forget', 'out', 'c_tilde']}
+        if not args.perplexity:
+            vectors = {k: [np.zeros((model.nhid*model.nlayers, len(s))) for s in tqdm(sentences)] for k in ['gates.in', 'gates.forget', 'gates.out', 'gates.c_tilde', 'hidden', 'cell']} #np.zeros((len(sentences), model.nhid*model.nlayers, max_length)) for k in ['in', 'forget', 'out', 'c_tilde']}
 
     for i, s in enumerate(tqdm(sentences)):
         #sys.stdout.write("{}% complete ({} / {})\r".format(int(i/len(sentences) * 100), i, len(sentences)))
@@ -99,46 +110,52 @@ if 'lstm' in args.get_representations:
         out, hidden = model(inp, hidden)
         out = torch.nn.functional.log_softmax(out[0]).unsqueeze(0)
         for j, w in enumerate(s):
+            if w not in vocab.word2idx and args.use_unk:
+                w = '<unk>'
             # store the surprisal for the current word
             log_probabilities[i][j] = out[0,0,vocab.word2idx[w]].data[0]
-
             inp = torch.autograd.Variable(torch.LongTensor([[vocab.word2idx[w]]]))
             if args.cuda:
                 inp = inp.cuda()
             out, hidden = model(inp, hidden)
             out = torch.nn.functional.log_softmax(out[0]).unsqueeze(0)
 
-            vectors['hidden'][i][:,j] = h[0].data.view(1,1,-1).cpu().numpy()
-            vectors['cell'][i][:,j] = h[1].data.view(1,1,-1).cpu().numpy()
-            # we can retrieve the gates thanks to the hacked function
-            for k, gates_k in vectors.items():
-                gates_k[i][:,j] = torch.cat([g[k].data for g in model.rnn.last_gates],1).cpu().numpy()
+            if not args.perplexity:
+                vectors['hidden'][i][:,j] = hidden[0].data.view(1,1,-1).cpu().numpy()
+                vectors['cell'][i][:,j] = hidden[1].data.view(1,1,-1).cpu().numpy()
+                # we can retrieve the gates thanks to the hacked function
+                for k, gates_k in vectors.items():
+                    if 'gates' in k:
+                        k = k.split('.')[1]
+                        gates_k[i][:,j] = torch.cat([g[k].data for g in model.rnn.last_gates],1).cpu().numpy()
         # save the results
-        out['log_probabilities'] = log_probabilities
+        saved['log_probabilities'] = log_probabilities
 
         if args.format != 'hdf5':
-            out['sentences'] = sentences
+            saved['sentences'] = sentences
 
-        for k, g in vectors.items():
-            out['gates.{}'.format(k)] = g
-        out['sentence_length'] = np.array(sentence_length)
+        saved['sentence_length'] = np.array(sentence_length)
 
-if args.perplexity:
-    print ("{}".format(log_probabilities.shape))
-    print ("{}".format(
-            math.exp(-log_probabilities.sum()/(log_probabilities!=0).sum())))
-else:
+        if not args.perplexity:
+            for k, g in vectors.items():
+                saved[k] = g
+
+    print ("Perplexity: {:.2f}".format(
+            math.exp(
+                    sum(-lp.sum() for lp in log_probabilities)/
+                    sum((lp!=0).sum() for lp in log_probabilities))))
+if not args.perplexity:
     #print ("DONE. Perplexity: {}".format(
     #        math.exp(-log_probabilities.sum()/((log_probabilities!=0).sum()))))
 
 
     if args.format == 'npz':
-        np.savez(args.output, **out)
+        np.savez(args.output, **saved)
     elif args.format == 'hdf5':
         with h5py.File("{}.h5".format(args.output), "w") as hf:
-            for k,v in out.items():
+            for k,v in saved.items():
                 dset = hf.create_dataset(k, data=v)
     elif args.format == 'pkl':
         with open(args.output + '.pkl', 'wb') as fout:
-            pickle.dump(out, fout, -1)
+            pickle.dump(saved, fout, -1)
 
