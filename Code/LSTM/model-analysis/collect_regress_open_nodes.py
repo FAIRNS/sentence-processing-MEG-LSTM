c61d80f6cb1afcd4950af4f8823ddbc791009add
FAIRNS <yair.lakretz@cea.fr>
2018-05-17 17:07:09 +0200

    minor

--- Code/LSTM/model-analysis/collect_regress_open_nodes.py
+++ Code/LSTM/model-analysis/collect_regress_open_nodes.py
@@ -22,7 +22,6 @@ bar_plot_width = 0.35  # the width of the bars
 models_names = ['model_ridge'] # 'model_lasso', 'model_ridge'
 # ----- Load LASSO model -----
 for i, model in enumerate(models_names):
-    fig, axs = plt.subplots(len(models_names), sharex=True, sharey=True)
     best_weights = []
     for seed in range(1,6,1):
         if model == 'model_ridge':
@@ -51,11 +50,19 @@ for i, model in enumerate(models_names):
 
     best_weights = np.vstack(best_weights) # stack back to ndarray
     ind = np.arange(best_weights.shape[1]) + 1  # the x locations for the groups
-    axs.bar(ind, np.mean(best_weights, axis=0), bar_plot_width, color='g', yerr=np.std(best_weights, axis=0))
+
+    fig, axs = plt.subplots(len(models_names), sharex=True, sharey=True)
+    axs.bar(ind, np.mean(best_weights, axis=0), bar_plot_width, color='b', yerr=np.std(best_weights, axis=0))
     axs.set_ylabel('Weight size')
     axs.set_xlabel('Unit')
     axs.set_xlim([0, best_weights.shape[1]])
     axs.set_title(model)
+    plt.show()
+
+    file_name = model + '_best_coef_' + settings.y_label + '_MODEL_' + settings.LSTM_pretrained_model + '_h_or_c_' + str(
+        settings.h_or_c) + '.png'
+    plt.savefig(op.join(settings.path2figures, file_name))
+    plt.close(fig)
 
     # pr.plot_weights(best_weights, model, axs, i, settings, params)
 
@@ -67,6 +74,3 @@ for i, model in enumerate(models_names):
     np.savetxt(op.join(settings.path2output, file_name), best_weights_sorted, fmt='%1.2f, %i')
     print('Saved to: ' + op.join(settings.path2output, file_name))
 
-    file_name = model + '_best_coef_' + settings.y_label + '_MODEL_' + settings.LSTM_pretrained_model + '_h_or_c_' + str(settings.h_or_c) + '.png'
-    plt.savefig(op.join(settings.path2figures, file_name))
-    plt.close(fig)
