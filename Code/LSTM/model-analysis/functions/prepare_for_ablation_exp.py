ee683831ee82fb6db1ccf1571f9761fabdefbef3
FAIRNS <yair.lakretz@cea.fr>
2018-06-26 11:01:09 +0200

    minor

--- Code/LSTM/model-analysis/functions/prepare_for_ablation_exp.py
+++ Code/LSTM/model-analysis/functions/prepare_for_ablation_exp.py
@@ -16,16 +16,31 @@ def get_VIF_values(design_matrix, thresh = 3):
     return VIF_values, IX_filter, ave_features, std_features
 
 
-def get_weight_outliers(weights):
+def get_weight_outliers(weights, thresh = 3):
+    import math
     import numpy as np
     ave = np.mean(weights, axis=0)
     std = np.std(weights, axis=0)
 
-    IX_pos = weights > ave + 3 * std
-    IX_neg = weights < ave - 3 * std
-    IX = IX_pos or IX_neg
+    IX_pos = weights > ave + thresh * std
+    IX_neg = weights < ave - thresh * std
+    IX = [x or y for x, y in zip(IX_pos, IX_neg)]
 
-    k = IX.sum()
+    k = sum(IX)
+    if k == 0:
+        print('No outliers were identified - change thresh')
+        n = 0
+    else:
+        # Find n such the n over k ~= 1000
+        n = k
+        n_over_k = 0
+        while n_over_k < 1000:
+            n += 1
+            n_over_k = math.factorial(n)/(math.factorial(k)*math.factorial(n - k))
 
+        # Check if previous n was closer to 10^3
+        previous_n_over_k = math.factorial(n-1)/(math.factorial(k)*math.factorial(n-k-1))
+        if np.abs(previous_n_over_k-1000)<np.abs(n_over_k-1000):
+            n = n-1
 
     return k, n, ave, std
