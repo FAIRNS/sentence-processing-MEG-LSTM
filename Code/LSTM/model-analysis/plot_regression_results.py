e2b0c450652bcdbf4b31631c2a08233ee8f69d4a
FAIRNS <yair.lakretz@cea.fr>
2018-06-26 11:01:09 +0200

    minor

--- Code/LSTM/model-analysis/plot_regression_results.py
+++ Code/LSTM/model-analysis/plot_regression_results.py
@@ -30,12 +30,35 @@ elif settings.which_layer == 1:
 elif settings.which_layer == 2:
     layer = 'second layer'
 
+with open(op.join(settings.path2output, 'ablation_scores.txt'), 'r') as f:
+    ablation = f.readlines()
+    ablation = [s.rstrip().split(' ') for s in ablation]
+ablation = np.asarray(ablation).astype(int)
+IX_ablation = [l[1] for l in np.argsort(ablation, axis=0)]
+
+
+file_name_regress_from_word_position = 'Ridge_Regression_number_of_open_nodes_from_word_position_all.pckl'
+
+with open(op.join(settings.path2output, file_name_regress_from_word_position), 'rb') as f:
+    results_regress_from_word_position = pickle.load(f, encoding='latin1')
+
+R_squared = []
+for seed in range(1,6,1):
+    file_name_regress_from_LSTM_to_residuals = 'Ridge_Regression_number_of_open_nodes_after_partial_out_word_position_' + settings.y_label + '_MODEL_' + settings.LSTM_pretrained_model + '_layer_' + str(settings.which_layer) + '_h_or_c_' + str(
+                    settings.h_or_c) + '_seed_' + str(seed) + '.pckl'
+
+    # filename_non_regularized_regression_weights = 'weights_non_regularized.pkl'
+    with open(op.join(settings.path2output, file_name_regress_from_LSTM_to_residuals), 'rb') as f:
+        results_regress_from_LSTM_to_residuals = pickle.load(f, encoding='latin1')
+    R_squared.append(results_regress_from_LSTM_to_residuals['ridge_scores_test'])
+print(np.mean(R_squared), np.std(R_squared))
+
+
 file_name_best_weights_model_from_LSTM_to_residuals = 'model_ridge_best_coef_all_MODEL_hidden650_batch128_dropout0.2_lr20.0.cpu.pt_h_or_c_0_layer_0.pkl'
 with open(op.join(settings.path2output, file_name_best_weights_model_from_LSTM_to_residuals), 'rb') as f:
     best_weights_model_from_LSTM_to_residuals = pickle.load(f, encoding='latin1')
 
-from functions import prepare_for_ablation_exp as pfa
-k, n, ave, std = pfa.get_weight_outliers(best_weights_model_from_LSTM_to_residuals[0])
+
 print('weights')
 
 # fig, ax = plt.subplots(1, 1)
