b529497cde16ff74b80a2dcc428e8412bd6738ff
FAIRNS <yair.lakretz@cea.fr>
2018-06-27 14:27:59 +0200

    minor

--- Code/MEG/PCA_analyze_weight.py
+++ Code/MEG/PCA_analyze_weight.py
@@ -9,25 +9,37 @@ settings = lsp.settings()
 params = lsp.params()
 
 vector_types = ['hidden', 'cell', 'word_vectors', 'bow_vectors']
+vector_types = ['hidden']
 for vector_type in vector_types:
     file_name = 'PCA_LSTM_' + vector_type + settings.LSTM_file_name + '.pkl'
     with open(op.join(settings.path2figures, 'units_activation', file_name), 'rb') as f:
         PCA_results = pickle.load(f)
-    PCs = PCA_results[0].components_
+    PCs = PCA_results.components_
 
     fig, axs = plt.subplots(2, 1)
     PC1 = PCs[0, :]
-    IX1 = np.argsort(np.abs(PC1))
+    IX1 = np.argsort(np.abs(PC1))[::-1]
     PC1_sorted = PC1[IX1]
+
+    file_name =  'PC1_LSTM_weights_' + vector_type + settings.LSTM_file_name + '.txt'
+    np.savetxt(op.join(settings.path2figures, 'units_activation', file_name), np.vstack([IX1, PC1_sorted]).T)
+
+    PC2 = PCs[1, :]
+    IX2 = np.argsort(np.abs(PC2))[::-1]
+    PC2_sorted = PC2[IX2]
+
+    file_name = 'PC2_LSTM_weights_' + vector_type + settings.LSTM_file_name + '.txt'
+    np.savetxt(op.join(settings.path2figures, 'units_activation', file_name), np.vstack([IX2, PC2_sorted]).T, fmt='%i, %1.2f')
+    print('Weights Saved to: ' + op.join(settings.path2figures, 'units_activation', file_name))
+
+    # Plot
+    # PC1
     axs[0].bar(range(1000), PC1_sorted)
     axs[0].set_title('PC1')
     axs[0].set_xticks(range(0,1000,30))
     axs[0].set_xticklabels(IX1[0:1000:30], rotation = 90)
     axs[0].set_xticklabels([])
-
-    PC2 = PCs[1, :]
-    IX2 = np.argsort(np.abs(PC2))
-    PC2_sorted = PC2[IX2]
+    # PC2
     axs[1].bar(range(1000), PC2_sorted)
     axs[1].set_title('PC2')
     axs[1].set_xticklabels(IX2)
