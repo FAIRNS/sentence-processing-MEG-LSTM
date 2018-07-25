eb72b4eb253492691a0a208324f0246cd7ebfe0f
Dieuwke <dieuwkehupkes@gmail.com>
2018-07-25 14:41:42 +0200

    Change default storage for filtering

--- Code/LSTM/model-analysis/functions/annotated_data.py
+++ Code/LSTM/model-analysis/functions/annotated_data.py
@@ -86,7 +86,7 @@ class Data(object):
         raise NotImplementedError
 
 
-    def filter(self, key='length', elements=range(8), n=50, set_as_attr=False):
+    def filter(self, key='length', elements=range(8), n=50, set_as_attr=True):
         """
         Filter the data such that it contains 50 instances of each
         element in elements (or as many as present in the data), where
