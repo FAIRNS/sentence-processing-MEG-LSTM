92f64243533b5dd9642675589f6ea1968b256362
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2018-09-03 16:20:56 +0200

    set correct value of k

--- Code/LSTM/model-analysis/generate_ablation_scripts.sh
+++ Code/LSTM/model-analysis/generate_ablation_scripts.sh
@@ -2,5 +2,5 @@ basefolder=$1
 
 for k in {10..21}
 do
-  python3 run-ablation.py -n 1 -k 10 -base_folder $basefolder > abl_scripts/abl_exp_k$k.sh
+  python3 run-ablation.py -n 1000 -k 10 -base_folder $basefolder > abl_scripts/abl_exp_k$k.sh
 done
