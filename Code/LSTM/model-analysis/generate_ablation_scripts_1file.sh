9b4e910fcef01af2f87411f4fb364bf29126a3c9
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2018-09-05 16:08:58 +0200

    fix writing error

--- Code/LSTM/model-analysis/generate_ablation_scripts_1file.sh
+++ Code/LSTM/model-analysis/generate_ablation_scripts_1file.sh
@@ -4,5 +4,5 @@ basefolder=$1
 
 for k in {10..21}
 do
-  python3 run-ablation.py -n 1000 -k 10 -base_folder $basefolder >> abl_scripts/all_abl_exp.sh
+  python3 run-ablation.py -n 1000 -k $k -base_folder $basefolder >> abl_scripts/all_abl_exp.sh
 done
