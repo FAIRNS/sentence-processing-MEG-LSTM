fcc95cc79de7ff70275b27bbad681c178ad5dbe4
FAIRNS <yair.lakretz@cea.fr>
2018-09-10 21:10:29 +0200

    argparse plot activations

--- Code/MEG/functions/load_settings_params.py
+++ Code/MEG/functions/load_settings_params.py
@@ -6,33 +6,26 @@ class settings:
 
         # General
         self.patient = 'am150105'
-        self.stimuli_type = 'Relative_clauses' # MEG_stimuli, NP_VP_transition, Relative_clauses
+        self.stimuli_type = 'from_marco_script' # MEG_stimuli, NP_VP_transition, Relative_clauses
 
         # Paths
-        base_folder = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM'
-        # base_folder = '/neurospin/unicog/protocols/intracranial/FAIRNS/sentence-processing-MEG-LSTM'
+        #base_folder = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM'
+        base_folder = '/neurospin/unicog/protocols/intracranial/FAIRNS/sentence-processing-MEG-LSTM'
 
-        self.path2code = '/private/home/germank/projects/neurospin/sentence-processing-MEG-LSTM/Code/MEG'
         self.path2code = op.join('..', '..', 'Code', 'MEG')
-
         self.path2MEGdata = op.join('..', '..', 'Data', 'MEG', self.patient)
 
         # FRENCH
-        self.path2LSTMdata = op.join('..', '..', 'Data', 'LSTM', 'activations', 'french', 'model2-500-2-0.5-SGD-10-tied.False-300')
-        self.path2LSTMdata = op.join(base_folder, 'Data', 'LSTM', 'activations', 'french', 'model2-500-2-0.5-SGD-10-tied.False-300')
+        #self.path2LSTMdata = op.join(base_folder, 'Data', 'LSTM', 'activations', 'french', 'model2-500-2-0.5-SGD-10-tied.False-300')
+        self.path2LSTMdata = op.join('..', '..', 'Data', 'LSTM')
 
         # ENGLISH
-        self.path2LSTMdata = op.join('..', '..', 'Data', 'LSTM', 'activations', 'english')
-
-        self.path2figures = op.join('..', '..', 'Figures')
+        #self.path2figures = op.join('..', '..', 'Figures')
         self.path2figures = op.join(base_folder, 'Figures')
 
-        self.path2output = op.join('..', '..', 'Output')
+        #self.path2output = op.join('..', '..', 'Output')
         self.path2output = op.join(base_folder, 'Output')
 
-        # self.path2stimuli = '/neurospin/meg/meg_tmp/sentcomp_Marti_2016/1-' + self.patient + '/Stim/data'
-        #self.path2stimuli = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/MEG/' + self.patient + '/Stim/data'
-        #self.path2stimuli = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/Stimuli' # New generated sentences
         # self.path2stimuli = op.join('..', '..', 'Data', 'Stimuli')
         self.path2stimuli = op.join(base_folder, 'Data', 'Stimuli')
 
@@ -40,43 +33,15 @@ class settings:
         #self.path2stimuli_parent = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/MEG/' + self.patient
 
         # Files
+        self.stimuli_file_name = 'RC_english_synthetic_with_objdep.txt' # Sentence list (text file)
+        self.stimuli_meta_data = 'info_RC_english_synthetic_with_objdep.p' # Corresponding meta-data (pickle)
+        self.LSTM_file_name = 'RC_english_synthetic_with_objdep.pkl' # LSTM activations for the sentences (pickle)
+        
         self.raw_file_name = self.patient + '_speed4_V2-epo.fif'
-
-        #self.stimuli_file_name = 'patient_' + self.patient + '_stimuli_anomaly_type_0'
-        #self.stimuli_file_name = 'NP_VP_transition.txt'
-        #self.stimuli_file_name = 'relative_clauses_pos_French.txt'
-        # self.stimuli_file_name = 'relative_clause_English.txt'
-        # self.stimuli_file_name = 'NP_VP_and_RC_filtered_perp.txt'
-        self.stimuli_file_name = 'objrel_subjrel_that.txt'
-        self.stimuli_file_name = 'RC_english_synthetic_with_objdep_adj.txt'
-
-        #self.stimuli_file_name = 'NP_VP_transition.utf-8.txt'
-        #self.stimuli_meta_data = 'info.p'
-        #self.stimuli_meta_data = 'info_RC_pos_French.p'
-        # self.stimuli_meta_data = 'info_RC_English.p'
-        self.stimuli_meta_data = 'info_NP_VP_and_RC_filtered_perp.p'
-        self.stimuli_meta_data = 'info_objrel_subjrel_that.p'
-        self.stimuli_meta_data = 'info_RC_english_synthetic_with_objdep_adj.p'
-
-
         self.MEG_file_name = 'patient_' + self.patient + '_epochs_lock_to_beginning_of_sentence_anomaly_type_0.npy'
         self.MEG_file_name = 'am150105_speed4_V2 - epo.fif'
-
-        #self.LSTM_file_name = 'vectors-LSTM1000-0.npy'
-        self.LSTM_file_name = 'vectors-LSTM500_2-0.npz'
-        self.LSTM_file_name = 'vectors-LSTM-500-2-0.5-SGD-10-tied.False-300-0-new-2.pkl'
-        self.LSTM_file_name = 'vectors-LSTM-500-2-0.5-SGD-10-tied.False-300-0.pkl'
-        # self.LSTM_file_name = 'NP_VP_transition.pkl'
-        # self.LSTM_file_name = 'relative_clauses.pkl'
-        # self.LSTM_file_name = 'relative_clauses_pos_French.pkl'
-        # self.LSTM_file_name = 'relative_clauses_English.pkl'
-        self.LSTM_file_name = 'NP_VP_and_RC_filtered_perp.pkl'
-        self.LSTM_file_name = 'objrel_subjrel_that.pkl'
-        self.LSTM_file_name = 'RC_english_synthetic_with_objdep_adj.pkl'
-
         self.word_vectors_file_name = 'word_vectors.npy'
         self.word_vectors_BOW_file_name = 'bow_vectors.npy'
-
         self.bad_trials_file_name = '/neurospin/meg/meg_tmp/sentcomp_Marti_2016/Seb/data/epochs/bad_epochs_' + self.patient + '_speed4_V3-epo.npy'
         self.bad_trials_file_name = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/MEG/am150105/bad_epochs_' + self.patient + '_speed4_V3-epo.npy'
 
