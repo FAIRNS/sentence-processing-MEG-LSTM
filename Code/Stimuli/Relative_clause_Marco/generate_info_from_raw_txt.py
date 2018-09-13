e2273f340208b2452daa6202dfd6f87c77ec3261
FAIRNS <yair.lakretz@cea.fr>
2018-09-13 16:41:52 +0200

    script from Marco to Tal's data format

--- Code/Stimuli/Relative_clause_Marco/generate_info_from_raw_txt.py
+++ Code/Stimuli/Relative_clause_Marco/generate_info_from_raw_txt.py
@@ -1,13 +1,24 @@
-path2input_text = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/Stimuli/objrel_objrel_that_subjrel_that.txt'
-path2output_sentences = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/Stimuli/RC_english_marco.txt'
-path2output_info = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/Stimuli/info_RC_english_marco.p'
+# This script parse Marco's sentences + metadata files into separate files: sentences, Theo's pkl meta file and
+# Tal's meta file.
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Switching from Marco files to Tal format')
+parser.add_argument('-i', '--input', required=True, help='Input sentences in Marco\'s format')
+parser.add_argument('-s', '--sentences', required=True, help='Output sentences in Tal\'s format')
+parser.add_argument('-o', '--info', required=True, help='Output info pickle file in Theo\'s format')
+parser.add_argument('-ts', '--tal_sentences', required=True, help='Output meta file for agreement task in Tal\'s format')
+parser.add_argument('-tm', '--tal_meta', required=True, help='Output meta file for agreement task in Tal\'s format')
+args = parser.parse_args()
+
 keep_sentences = [['subjrel_that', 'singular', 'singular'], ['subjrel_that', 'plural', 'singular'], ['subjrel_that', 'plural', 'plural'], ['objrel', 'plural', 'plural'], ['objrel_that', 'plural', 'plural']]
 # Open raw text file from Marco's sentence generator
-with open(path2input_text, 'r') as f:
+with open(args.input, 'r') as f:
     raw_sentences = f.readlines()
 
 # Split each row to different fields and save separately the sentences and the info
 sentences = [line.split('\t')[1]+ '\n' for line in raw_sentences]
+# For Theo's format:
 info = []
 for line in raw_sentences:
     curr_info = {}
@@ -19,10 +30,25 @@ for line in raw_sentences:
     if len(curr_line)>4:
         curr_info['number_3'] = curr_line[4]
     if len(curr_line) > 5:
-        curr_info['verb_1'] = curr_line[5]
+        curr_info['verb_1_wrong'] = curr_line[5]
     if len(curr_line) > 6:
-        curr_info['verb_2'] = curr_line[6]
+        curr_info['verb_2_wrong'] = curr_line[6]
     info.append(curr_info)
+
+# Prepare in Tal's format:
+sentences_Tal = []; gold_Tal = []
+for line in raw_sentences:
+    curr_line = line.split('\t')
+    if curr_line[0] == 'objrel': # analyzing only objrel for 1149
+        verb1_position = '4'
+        verb2_position = 5
+        verb2_correct = curr_line[1].split(' ')[verb2_position].strip()
+        verb2_wrong = curr_line[6].strip()
+        num_attributes = '-999'
+        line_Tal = str(verb2_position) + '\t' + verb2_correct + '\t' + verb2_wrong + '\t' + num_attributes + '\n'
+
+        sentences_Tal.append(curr_line[1] + '\n')
+        gold_Tal.append(line_Tal)
 
 # Filter certain sentence types if desired.
 if keep_sentences:
@@ -37,15 +63,28 @@ if keep_sentences:
     sentences = [curr_sentence for IX, curr_sentence in enumerate(sentences) if IX in IX_to_keep]
     info = [curr_info for IX, curr_info in enumerate(info) if IX in IX_to_keep]
 
-
-
-# Save info list to drive
 import pickle
-with open(path2output_sentences, 'w') as f:
+# Save sentences
+with open(args.sentences, 'w') as f:
     f.writelines(sentences)
 
-with open(path2output_info, 'wb') as f:
+# Save info in Theo's format
+with open(args.info, 'wb') as f:
     pickle.dump(info, f)
+
+# Save sentences for Tal's agreement task
+with open(args.tal_sentences, 'w') as f:
+    f.writelines(sentences_Tal)
+
+# Save meta for Tal's agreement task
+with open(args.tal_meta, 'w') as f:
+    f.writelines(gold_Tal)
+
+# ------------------------------------------------------------
+# path2input_text = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/Stimuli/objrel_objrel_that_subjrel_that.txt'
+# path2output_sentences = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/Stimuli/RC_english_marco.txt'
+# path2output_info = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/Stimuli/info_RC_english_marco.p'
+
 
 # path2input_text = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/Stimuli/RC_double_subjrel_that_english_marco.txt'
 # path2output_sentences = '/home/yl254115/Projects/FAIRNS/sentence-processing-MEG-LSTM/Data/Stimuli/RC_double_english_marco.txt'
