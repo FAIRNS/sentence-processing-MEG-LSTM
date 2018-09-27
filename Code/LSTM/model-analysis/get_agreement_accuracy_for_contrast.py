4f5c76d0799e1f083caf9463c797f8e057f9ae51
FAIRNS <yair.lakretz@cea.fr>
2018-09-27 12:26:47 +0200

    p-difference added to get_agreement

--- Code/LSTM/model-analysis/get_agreement_accuracy_for_contrast.py
+++ Code/LSTM/model-analysis/get_agreement_accuracy_for_contrast.py
@@ -13,7 +13,7 @@ print(condition_constraints)
 ablation_results = pickle.load(open(args.ablation_results, 'rb'))
 info = pickle.load(open(args.info, 'rb'))
 
-success = []
+success = []; p_difference = []
 for i, sentence_info in enumerate(info):
     check_if_all_constraints_are_met = True
     for constraint in condition_constraints:
@@ -21,7 +21,9 @@ for i, sentence_info in enumerate(info):
         if sentence_info[key] != value: check_if_all_constraints_are_met = False
     if check_if_all_constraints_are_met:
         success.append(ablation_results['log_p_targets_correct'][i]>ablation_results['log_p_targets_wrong'][i])
+        p_difference.append(np.exp(ablation_results['log_p_targets_correct'][i])-np.exp(ablation_results['log_p_targets_wrong'][i]))
 if success:
     print(np.mean(success))
+    print('p-difference: %1.5f +- %1.5f' % (np.mean(p_difference), np.std(p_difference)))
 else:
     print('No sentences meet the keys-values in the condition. Check for typos and verify the meta info file include them')
