8eed08c1cb246cd0b1f865b8245d33403ebe4f39
Yair LAKRETZ <yl254115@is153802.intra.cea.fr>
2018-10-07 02:49:41 +0200

    minors

--- Code/LSTM/model-analysis/run_pipeline.py
+++ Code/LSTM/model-analysis/run_pipeline.py
@@ -8,9 +8,9 @@ print('Code/Stimuli/Relative_clause_Marco/generate_relatives.pl 1 %s %s > Data/S
 
 print('python3 Code/Stimuli/Relative_clause_Marco/generate_info_from_raw_txt.py -i Data/Stimuli/%s.txt -o Data/Stimuli/%s\n\n' % (args.condition, args.condition))
 
-print('python3 Code/LSTM/model-analysis/extract-activations.py Data/LSTM/hidden650_batch128_dropout0.2_lr20.0.cpu.pt --input Data/Stimuli/%s.text --vocabulary Data/LSTM/english_vocab.txt --output Data/LSTM/%s --eos-separator "<eos>" --format pkl --use-unk --unk-token "<unk>" --lang en -g lstm\n\n' % (args.condition, args.condition))
+print('python3 Code/LSTM/model-analysis/extract-activations.py Data/LSTM/hidden650_batch128_dropout0.2_lr20.0.pt --input Data/Stimuli/%s.text --vocabulary Data/LSTM/english_vocab.txt --output Data/LSTM/%s --eos-separator "<eos>" --format pkl --use-unk --unk-token "<unk>" --lang en -g lstm --cuda\n\n' % (args.condition, args.condition))
 
-print('python3 Code/LSTM/model-analysis/ablation-experiment.py Data/LSTM/hidden650_batch128_dropout0.2_lr20.0.cpu.pt --input Data/Stimuli/%s --vocabulary Data/LSTM/english_vocab.txt --output Output/%s --eos-separator "<eos>" --format pkl -u 1 -u 750 -g 1 -s 1\n\n' % (args.condition, args.condition))
+print('python3 Code/LSTM/model-analysis/ablation-experiment.py Data/LSTM/hidden650_batch128_dropout0.2_lr20.0.pt --input Data/Stimuli/%s --vocabulary Data/LSTM/english_vocab.txt --output Output/%s --eos-separator "<eos>" --format pkl -u 1 -u 750 -g 1 -s 1 --cuda\n\n' % (args.condition, args.condition))
 
 print('python3 Code/Stimuli/Relative_clause_Marco/add_success_and_perplexity_to_info.py -i Data/Stimuli/%s.info -r Output/%s.abl -a Data/LSTM/%s.pkl\n\n' % (args.condition, args.condition, args.condition))
 
