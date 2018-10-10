187e1dc9c3273ad7d8aca46ba81e7ebbf7ef4bc1
FAIRNS <yair.lakretz@cea.fr>
2018-10-10 19:44:31 +0200

    PCA and output weights visuablization

--- Code/LSTM/model-analysis/extract_embeddings_from_rnn.py
+++ Code/LSTM/model-analysis/extract_embeddings_from_rnn.py
@@ -9,16 +9,21 @@ import pickle
 import matplotlib.pyplot as plt
 import lstm
 import networkx as nx
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+	'../src/word_language_model')))
+import data
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='Extract and plot LSTM weights')
 parser.add_argument('-model', type=str, help='Meta file stored once finished training the corpus')
-parser.add_argument('-v', '--vocabulary', default='reduced_vocab.txt')
-parser.add_argument('-o', '--output', default='Figures/embedding_PCA.png', help='Destination for the output weights')
-parser.add_argument('--cuda', action='store_true', default=False)
+parser.add_argument('-v', '--vocabulary', default='Data/LSTM/english_vocab.txt')
+parser.add_argument('-o', '--output', default='Figures/verbs.png', help='Destination for the output figure')
+parser.add_argument('-u', '--units', nargs='+', help='Destination for the output figure')
 args = parser.parse_args()
 
 # os.makedirs(os.path.dirname(args.output), exist_ok=True)
-
+dirname = os.path.dirname(args.output)
+filename = os.path.basename(args.output)
 
 gate_names = ['Input', 'Forget', 'Cell', 'Output']
 # Load model
@@ -26,17 +31,100 @@ print('Loading models...')
 print('\nmodel: ' + args.model+'\n')
 model = torch.load(args.model)
 model.rnn.flatten_parameters()
-embeddings = model.encoder.weight.data.numpy()
+embeddings = model.decoder.weight.data.numpy()
+vocab = data.Dictionary(args.vocabulary)
 
+
+### Verbs
+
+verbs_singular = ["admires",
+			   "approves",
+			   "avoids",
+			   "confuses",
+			   "criticizes",
+			   "discourages",
+			   "encourages",
+			   "engages",
+			   "greets",
+			   "inspires",
+			   "knows",
+			   "observes",
+			   "remembers",
+			   "stimulates",
+			   "understands", "believes",
+				  "says",
+				  "thinks"]
+
+verbs_plural = ["admire",
+			 "approve",
+			 "avoid",
+			 "confuse",
+			 "criticize",
+			 "discourage",
+			 "encourage",
+			 "engage",
+			 "greet",
+			 "inspire",
+			 "know",
+			 "observe",
+			 "remember",
+			 "stimulate",
+			 "understand", "believe",
+				"say",
+				"think"]
+
+verbs_all = verbs_singular + verbs_plural
+####
+idx_verbs_singular = [vocab.word2idx[w] for w in verbs_singular]
+idx_verbs_plural = [vocab.word2idx[w] for w in verbs_plural]
+idx_verbs_all = idx_verbs_singular + idx_verbs_plural
+
+embeddings_verbs_singular = embeddings[idx_verbs_singular, :]
+embeddings_verbs_plural = embeddings[idx_verbs_plural, :]
+embeddings_verbs_all = embeddings[idx_verbs_singular + idx_verbs_plural ,:]
+#### Plot verb embeddings
 from sklearn.decomposition import PCA
 
 pca = PCA(n_components=2)
-pca.fit(embeddings)
+pca.fit(embeddings_verbs_all)
 pca_score = pca.explained_variance_ratio_
 V = pca.components_
-X_transformed = pca.fit_transform(embeddings)
+X_transformed = pca.fit_transform(embeddings_verbs_all)
 
-fig, ax = plt.subplots(1, figsize = (8, 8))
-ax.text(X_transformed[:, 0], X_transformed[:, 1])
+fig, ax = plt.subplots(1, figsize = (40, 30))
+for i in tqdm(range(X_transformed.shape[0])):
+	# if w % 10 == 1:
+	ax.text(X_transformed[i, 0], X_transformed[i, 1], verbs_all[i], size=30)
 
-fig.savefig(args.output)
+lim_max = np.max(X_transformed)
+lim_min = np.min(X_transformed)
+ax.set_xlim((lim_min, lim_max))
+ax.set_ylim((lim_min, lim_max))
+ax.axis('off')
+fig.savefig(os.path.join(dirname, 'PCA_'+filename))
+plt.close(fig)
+
+units = [int(u) for u in args.units]
+bar_width = 0.2
+## Extract weights from number units to verbs
+fig, ax = plt.subplots(1, figsize = (10,10))
+for u, from_unit in enumerate(units):
+	if u == 0:
+		label_sing = 'Singular form of verb'; label_plur = 'Plural form of verb'
+	else:
+		label_sing = ''; label_plur = ''
+	from_unit = from_unit - 650
+	output_weights_singular = embeddings[idx_verbs_singular, from_unit]
+	ax.scatter(u + np.random.random(output_weights_singular.size) * bar_width - bar_width/2, output_weights_singular, s=10, color='b', label=label_sing)
+	output_weights_plural = embeddings[idx_verbs_plural, from_unit]
+	ax.scatter(u + np.random.random(output_weights_plural.size) * bar_width - bar_width/2, output_weights_plural, s=10, color='g', label=label_plur)
+
+
+plt.legend()
+plt.xticks(range(len(units)), [str(u) for u in units])
+ax.set_ylabel('Size of output weight', fontsize = 16)
+ax.set_xlabel('Unit', fontsize = 16)
+ax.axhline(linewidth=2, color='k', ls = '--')
+fig.savefig(os.path.join(dirname, 'weight_dists_'+filename))
+plt.close(fig)
+
