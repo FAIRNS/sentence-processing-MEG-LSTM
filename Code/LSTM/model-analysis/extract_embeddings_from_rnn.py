0c18b72cb999c612c6e57fa85fa31bd7e869ef65
FAIRNS <yair.lakretz@cea.fr>
2018-10-12 16:02:50 +0200

    identify feature units from a text file contatinng two lists of words with contstraced feature

--- Code/LSTM/model-analysis/extract_embeddings_from_rnn.py
+++ Code/LSTM/model-analysis/extract_embeddings_from_rnn.py
@@ -5,27 +5,23 @@ import argparse
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../src/word_language_model')))
 import data
 import numpy as np
-import pickle
 import matplotlib.pyplot as plt
-import lstm
-import networkx as nx
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-	'../src/word_language_model')))
-import data
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='Extract and plot LSTM weights')
 parser.add_argument('-model', type=str, help='Meta file stored once finished training the corpus')
 parser.add_argument('-v', '--vocabulary', default='Data/LSTM/english_vocab.txt')
 parser.add_argument('-o', '--output', default='Figures/verbs.png', help='Destination for the output figure')
-parser.add_argument('-u', '--units', nargs='+', help='Destination for the output figure')
+parser.add_argument('-u', '--units', nargs='+', help='Which units to plot')
+parser.add_argument('-i', '--input', default='Data/Stimuli/singular_plural_verbs.txt', help='Text file with two tab delimited columns with the lists of output words to contrast with the PCA')
 args = parser.parse_args()
 
+gate_names = ['Input', 'Forget', 'Cell', 'Output']
+# Parse output dir and file names:
 # os.makedirs(os.path.dirname(args.output), exist_ok=True)
 dirname = os.path.dirname(args.output)
 filename = os.path.basename(args.output)
 
-gate_names = ['Input', 'Forget', 'Cell', 'Output']
 # Load model
 print('Loading models...')
 print('\nmodel: ' + args.model+'\n')
@@ -34,54 +30,22 @@ model.rnn.flatten_parameters()
 embeddings = model.decoder.weight.data.numpy()
 vocab = data.Dictionary(args.vocabulary)
 
-
-### Verbs
-
-verbs_singular = ["admires",
-			   "approves",
-			   "avoids",
-			   "confuses",
-			   "criticizes",
-			   "discourages",
-			   "encourages",
-			   "engages",
-			   "greets",
-			   "inspires",
-			   "knows",
-			   "observes",
-			   "remembers",
-			   "stimulates",
-			   "understands", "believes",
-				  "says",
-				  "thinks"]
-
-verbs_plural = ["admire",
-			 "approve",
-			 "avoid",
-			 "confuse",
-			 "criticize",
-			 "discourage",
-			 "encourage",
-			 "engage",
-			 "greet",
-			 "inspire",
-			 "know",
-			 "observe",
-			 "remember",
-			 "stimulate",
-			 "understand", "believe",
-				"say",
-				"think"]
-
+# Read list of contrasted words (e.g., singular vs. plural verbs).
+with open(args.input, 'r') as f:
+	lines=f.readlines()
+verbs_singular = [l.split('\t')[0].strip() for l in lines]
+verbs_plural = [l.split('\t')[1].strip() for l in lines]
 verbs_all = verbs_singular + verbs_plural
-####
+print(verbs_all)
+
+# Get index in the vocab for all words and extract embeddings
 idx_verbs_singular = [vocab.word2idx[w] for w in verbs_singular]
 idx_verbs_plural = [vocab.word2idx[w] for w in verbs_plural]
 idx_verbs_all = idx_verbs_singular + idx_verbs_plural
-
 embeddings_verbs_singular = embeddings[idx_verbs_singular, :]
 embeddings_verbs_plural = embeddings[idx_verbs_plural, :]
 embeddings_verbs_all = embeddings[idx_verbs_singular + idx_verbs_plural ,:]
+
 #### Plot verb embeddings
 from sklearn.decomposition import PCA
 
@@ -90,6 +54,10 @@ pca.fit(embeddings_verbs_all)
 pca_score = pca.explained_variance_ratio_
 V = pca.components_
 X_transformed = pca.fit_transform(embeddings_verbs_all)
+PC1 = V[0, :]
+PC2 = V[1, :]
+print(650 + np.argsort(np.negative(np.abs(PC1))))
+print(650 + np.argsort(np.negative(np.abs(PC2))))
 
 fig, ax = plt.subplots(1, figsize = (40, 30))
 for i in tqdm(range(X_transformed.shape[0])):
@@ -102,6 +70,7 @@ ax.set_xlim((lim_min, lim_max))
 ax.set_ylim((lim_min, lim_max))
 ax.axis('off')
 fig.savefig(os.path.join(dirname, 'PCA_'+filename))
+print('Saved to: ' + os.path.join(dirname, 'PCA_'+filename))
 plt.close(fig)
 
 units = [int(u) for u in args.units]
@@ -126,5 +95,6 @@ ax.set_ylabel('Size of output weight', fontsize = 16)
 ax.set_xlabel('Unit', fontsize = 16)
 ax.axhline(linewidth=2, color='k', ls = '--')
 fig.savefig(os.path.join(dirname, 'weight_dists_'+filename))
+print('saved to: ' + os.path.join(dirname, 'weight_dists_'+filename))
 plt.close(fig)
 
