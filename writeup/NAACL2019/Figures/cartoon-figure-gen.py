81a91e74e9b55e825d2b346ac47d80d611494bfc
Germán Kruszewski <germank@gmail.com>
2018-11-05 19:52:45 +0100

    lstm cartoon figure generator

--- writeup/NAACL2019/Figures/cartoon-figure-gen.py
+++ writeup/NAACL2019/Figures/cartoon-figure-gen.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-sentence = 'The girl(s) with blue eyes often goes'.split()
+sentence = 'The girl/girls with blue eyes often goes'.split()
 
 # define series values
 sing_input = [0]*len(sentence)
@@ -11,11 +11,10 @@ forget[0] = 0
 forget[1] = 0
 
 
-plt.figure(figsize=(7,5))
-plt.plot(sing_input, ls='--', lw=4, label='The girl $i_t \\tilde{C}_t$')
-plt.plot(pl_input, ls='--', lw=4, label='The girls $i_t \\tilde{C}_t$')
-plt.plot(forget, ls=':', lw=4, label='The girl(s) $f_t$')
-plt.xticks(ticks=range(len(sentence)), labels=sentence, fontsize=18, rotation=-45)
+plt.figure(figsize=(9,4))
+plt.plot(pl_input, ls='--', lw=4, label='The girls $i_t \\tilde{C}_t$', color='C1')
+plt.plot(sing_input, ls='--', lw=4, label='The girl $i_t \\tilde{C}_t$', color='C0')
+plt.plot(forget, ls=':', lw=4, label='The girl/girls $f_t$', color='C2')
+plt.xticks(ticks=range(len(sentence)), labels=sentence, fontsize=18)
 plt.legend(fontsize=18)
-plt.tight_layout()
 plt.savefig('lstm-cartoon.png', dpi=100)
