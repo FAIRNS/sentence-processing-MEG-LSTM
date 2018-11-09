28ed7c50400629d595309472c0064aa1a003450c
Germán Kruszewski <germank@gmail.com>
2018-11-09 17:21:03 +0100

    updated lstm activation cartoon

--- writeup/NAACL2019/Figures/cartoon-figure-gen.py
+++ writeup/NAACL2019/Figures/cartoon-figure-gen.py
@@ -1,20 +1,106 @@
 import matplotlib.pyplot as plt
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 
-sentence = 'The girl/girls with blue eyes often goes'.split()
+sentence = 'The \\textbf{athlete(s)} behind the \\underline{bike(s)} \\textbf{observe(s)}'.split()
+
+# nouns and verb positions
+N1=1
+N2=4
+V=5
+off = .1
+SS_off= off+off/2
+SP_off = off/2
+PS_off= -off/2
+PP_off = -off-off/2
+
 
 # define series values
-sing_input = [0]*len(sentence)
-sing_input[1] = 1
-pl_input = [0]*len(sentence)
-forget = [1] * len(sentence)
-forget[0] = 0
-forget[1] = 0
+SS_sugg = [0 + SS_off]*len(sentence)
+SS_sugg[N1] = 1 + SS_off
+SS_sugg[N2] = 1 + SS_off
+PS_sugg = [0 + PS_off]*len(sentence)
+PS_sugg[N1] = -1 + PS_off
+PS_sugg[N2] = 1 + PS_off
+SP_sugg = [0 + SP_off]*len(sentence)
+SP_sugg[N1] = 1 + SP_off
+SP_sugg[N2] = -1 + SP_off
+PP_sugg = [0 + PP_off]*len(sentence)
+PP_sugg[N1] = -1 + PP_off
+PP_sugg[N2] = -1 + PP_off
+
+SS_input = [0 + SS_off]*len(sentence)
+SS_input[N1] = 1 + SS_off
+SS_input[N2] = 0 + SS_off
+PS_input = [0 + PS_off]*len(sentence)
+PS_input[N1] = 1 + PS_off
+PS_input[N2] = 0 + PS_off
+SP_input = [0 + SP_off]*len(sentence)
+SP_input[N1] = 1 + SP_off
+SP_input[N2] = 0 + SP_off
+PP_input = [0 + PP_off]*len(sentence)
+PP_input[N1] = 1 + PP_off
+PP_input[N2] = 0 + PP_off
+
+SS_forget = [1 + SS_off]*len(sentence)
+SS_forget[N1] = 0 + SS_off
+SS_forget[N1-1] = 0 + SS_off
+PS_forget = [1 + PS_off]*len(sentence)
+PS_forget[N1] = 0 + PS_off
+PS_forget[N1-1] = 0 + PS_off
+SP_forget = [1 + SP_off]*len(sentence)
+SP_forget[N1] = 0 + SP_off
+SP_forget[N1-1] = 0 + SP_off
+PP_forget = [1 + PP_off]*len(sentence)
+PP_forget[N1] = 0 + PP_off
+PP_forget[N1-1] = 0 + PP_off
 
 
-plt.figure(figsize=(9,4))
-plt.plot(pl_input, ls='--', lw=4, label='The girls $i_t \\tilde{C}_t$', color='C1')
-plt.plot(sing_input, ls='--', lw=4, label='The girl $i_t \\tilde{C}_t$', color='C0')
-plt.plot(forget, ls=':', lw=4, label='The girl/girls $f_t$', color='C2')
-plt.xticks(ticks=range(len(sentence)), labels=sentence, fontsize=18)
-plt.legend(fontsize=18)
-plt.savefig('lstm-cartoon.png', dpi=100)
+SS_cell = [1 + SS_off]*len(sentence)
+SS_cell[N1] = 0 + SS_off
+SS_cell[N1-1] = 0 + SS_off
+PS_cell = [-1 + PS_off]*len(sentence)
+PS_cell[N1] = 0 + PS_off
+PS_cell[N1-1] = 0 + PS_off
+SP_cell = [1 + SP_off]*len(sentence)
+SP_cell[N1] = 0 + SP_off
+SP_cell[N1-1] = 0 + SP_off
+PP_cell = [-1 + PP_off]*len(sentence)
+PP_cell[N1] = 0 + PP_off
+PP_cell[N1-1] = 0 + PP_off
+
+
+plt.figure(figsize=(10,10))
+fig, (sugg_ax, input_ax, forget_ax, cell_ax) = plt.subplots(4, 1) # two plots in a single column
+sugg_ax.plot(PP_sugg, ls='-', lw=2, label=r'\textbf{Plural}-\underline{Plural}', color='blue')
+sugg_ax.plot(SS_sugg, ls='-', lw=2, label=r'\textbf{Singular}-\underline{Singular}', color='red')
+sugg_ax.plot(PS_sugg, ls=':', lw=3, label=r'\textbf{Plural}-\underline{Plural}', color='blue')
+sugg_ax.plot(SP_sugg, ls=':', lw=3, label=r'\textbf{Singular}-\underline{Singular}', color='red')
+sugg_ax.set_ylabel("Suggestion")
+sugg_ax.set_xticks([])
+
+input_ax.plot(PP_input, ls='-', lw=2, label='Plural-Plural', color='blue')
+input_ax.plot(SS_input, ls='-', lw=2, label='Singular-Singular', color='red')
+input_ax.plot(PS_input, ls=':', lw=3, label='Plural-Plural', color='blue')
+input_ax.plot(SP_input, ls=':', lw=3, label='Singular-Singular', color='red')
+input_ax.set_ylabel("Input", labelpad=12)
+input_ax.set_xticks([])
+
+forget_ax.plot(PP_forget, ls='-', lw=2, label='Plural-Plural', color='blue')
+forget_ax.plot(SS_forget, ls='-', lw=2, label='Singular-Singular', color='red')
+forget_ax.plot(PS_forget, ls=':', lw=3, label='Plural-Plural', color='blue')
+forget_ax.plot(SP_forget, ls=':', lw=3, label='Singular-Singular', color='red')
+forget_ax.set_ylabel("Forget", labelpad=12)
+forget_ax.set_xticks([])
+
+cell_ax.plot(PP_cell, ls='-', lw=2, label='Plural-Plural', color='blue')
+cell_ax.plot(SS_cell, ls='-', lw=2, label='Singular-Singular', color='red')
+cell_ax.plot(PS_cell, ls=':', lw=3, label='Plural-Plural', color='blue')
+cell_ax.plot(SP_cell, ls=':', lw=3, label='Singular-Singular', color='red')
+cell_ax.set_ylabel("Cell")
+#forget_ax.set_xticks([])
+#plt.plot(forget, ls=':', lw=4, label='The girl/girls $f_t$', color='C2')
+plt.xticks(ticks=range(len(sentence)), labels=sentence, fontsize=14, rotation=0)
+handles, labels = sugg_ax.get_legend_handles_labels()
+fig.legend(handles, labels, loc='upper center', ncol=2)
+plt.savefig('unit-timeseries-cartoon.pdf', dpi=100)
