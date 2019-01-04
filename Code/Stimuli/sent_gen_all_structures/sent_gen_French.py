174ab686d3a2596f02c1eb71db994d8603877d52
FAIRNS <yair.lakretz@cea.fr>
2019-01-04 10:40:16 +0100

    allow generating all types

--- Code/Stimuli/sent_gen_all_structures/sent_gen_French.py
+++ Code/Stimuli/sent_gen_all_structures/sent_gen_French.py
@@ -5,37 +5,41 @@ from lexicon import Words
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Stimulus generator for French')
-parser.add_argument('-t', '--type', default=0, type=int, help = '0-5, one of six types of sentences in Theo dataset')
+parser.add_argument('-t', '--type', default=all, type=str, help = '{0-5, all}: one of six types of sentences in Theo dataset. "all" for looping over all types')
 parser.add_argument('-n', '--natask', default='nounPP', type=str, help = 'Number-agreement (NA) task to generate')
 
 args = parser.parse_args()
-type = args.type
+if args.type == 'all':
+	types = range(6)
+else:
+	types = [int(args.type)]
 
 # Generate sentences and print to terminal
-if args.natask == 'nounPP':
-    for subject, gender, number in zip(Words[type]['subjects']['word'], Words[type]['subjects']['sexe'], Words[type]['subjects']['plurality']):
-        det_type = 'determinants' if number == 'sing' else 'determinants_plural'
-        for det in Words[type][det_type][gender]:
-            for gender_attractor in ['masc', 'femi']:
-                for number_attractor in ['sing', 'plur']:
-                    for PP in Words[type]['prepositions'][subject][gender_attractor][number_attractor]:
-                        NP = ' '.join([det] + [subject] + [PP])
-                        for tense in ['past', 'future']:
-                            for VP in Words[type]['verbs'][tense][number]:
-                                sentence = NP + ' ' + VP
-                                print('%s\t%s\t%s\t%s\t%s\t%s' % (sentence, tense, gender, number, gender_attractor, number_attractor))
+for type in types:
+	if args.natask == 'nounPP':
+		for subject, gender, number in zip(Words[type]['subjects']['word'], Words[type]['subjects']['sexe'], Words[type]['subjects']['plurality']):
+			det_type = 'determinants' if number == 'sing' else 'determinants_plural'
+			for det in Words[type][det_type][gender]:
+				for gender_attractor in ['masc', 'femi']:
+					for number_attractor in ['sing', 'plur']:
+						for PP in Words[type]['prepositions'][subject][gender_attractor][number_attractor]:
+							NP = ' '.join([det] + [subject] + [PP])
+							for tense in ['past', 'future']:
+								for VP in Words[type]['verbs'][tense][number]:
+									sentence = NP + ' ' + VP
+									print('%s\t%s\t%s\t%s\t%s\t%s' % (sentence, tense, gender, number, gender_attractor, number_attractor))
 
 
-if args.natask == 'nounPPAdj':
-    for subject, gender, number in zip(Words[type]['subjects']['word'], Words[type]['subjects']['sexe'], Words[type]['subjects']['plurality']):
-        det_type = 'determinants' if number == 'sing' else 'determinants_plural'
-        for det in Words[type][det_type][gender]:
-            for gender_attractor in ['masc', 'femi']:
-                for number_attractor in ['sing', 'plur']:
-                    for PP in Words[type]['prepositions'][subject][gender_attractor][number_attractor]:
-                        for adjectif in Words[type]['adjectives'][gender_attractor][number_attractor]:
-                            NP = ' '.join([det] + [subject] + [PP] + [adjectif])
-                            for tense in ['past', 'future']:
-                                for VP in Words[type]['verbs'][tense][number]:
-                                    sentence = NP + ' ' + VP
-                                    print('%s\t%s\t%s\t%s\t%s\t%s' % (sentence, tense, gender, number, gender_attractor, number_attractor))
+	if args.natask == 'nounPPAdj':
+		for subject, gender, number in zip(Words[type]['subjects']['word'], Words[type]['subjects']['sexe'], Words[type]['subjects']['plurality']):
+			det_type = 'determinants' if number == 'sing' else 'determinants_plural'
+			for det in Words[type][det_type][gender]:
+				for gender_attractor in ['masc', 'femi']:
+					for number_attractor in ['sing', 'plur']:
+						for PP in Words[type]['prepositions'][subject][gender_attractor][number_attractor]:
+							for adjectif in Words[type]['adjectives'][gender_attractor][number_attractor]:
+								NP = ' '.join([det] + [subject] + [PP] + [adjectif])
+								for tense in ['past', 'future']:
+									for VP in Words[type]['verbs'][tense][number]:
+										sentence = NP + ' ' + VP
+										print('%s\t%s\t%s\t%s\t%s\t%s' % (sentence, tense, gender, number, gender_attractor, number_attractor))
