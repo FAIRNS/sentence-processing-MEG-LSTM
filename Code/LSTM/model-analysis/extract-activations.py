f6b8030a2bd908d8147c75b3c3cac1e9737e1194
Yair LAKRETZ <yl254115@is153802.intra.cea.fr>
2019-02-23 02:19:45 +0100

    pipelines added

--- Code/LSTM/model-analysis/extract-activations.py
+++ Code/LSTM/model-analysis/extract-activations.py
@@ -142,6 +142,9 @@ if 'lstm' in args.get_representations:
                         "After the War of the Holy League in 1537 against the Ottoman Empire , a truce between Venice and the Ottomans was created in 1539 . <eos>",
                         "Moore says : \" Tony and I had a good <unk> and off-screen relationship , we are two very different people , but we did share a sense of humour \" . <eos>",
                         "<unk> is also the basis for online games sold through licensed lotteries . <eos>"])
+    elif args.lang == 'it':
+        init_sentence = " ".join(["L' <unk> o <unk> ( in francese , \" <unk> \" ) Ã¨ uno strumento musicale azionato con una tastiera , detta manuale . <eos>", "<unk> indiano . <eos>",
+        "Ma altre caratteristiche hanno fatto in modo che si <unk> ugualmente nel contesto della musica indiana ( anche di quella \" classica \" ) . <eos>"])
     else:
         # init_sentence = " ".join([
         # "hier , considéré avec scepticisme du fait de la présence du minitel , le réseau connaît aujourd'hui un véritable engouement . </s>",
