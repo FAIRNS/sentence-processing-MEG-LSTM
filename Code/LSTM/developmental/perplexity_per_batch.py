d82933b94977392400047cf1ed48fdd9d7d59af3
FAIRNS <yair.lakretz@cea.fr>
2019-03-14 14:06:20 +0100

    singular plural classification per batch

--- Code/LSTM/developmental/perplexity_per_batch.py
+++ Code/LSTM/developmental/perplexity_per_batch.py
@@ -4,7 +4,7 @@ import os, glob
 
 parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 RNN/LSTM Language Model')
 parser.add_argument('--val-data', type=str, default='../lm-training/data/en/wikitext-2/valid.txt', help='location of the data corpus')
-parser.add_argument('--path2models', type=str, default='../../../../../LSTM_training/word_language_model/output/', help='path to model')
+parser.add_argument('--path2models', type=str, default='../../../../../LSTM_training/word_language_model/output/', help='path to pt models for all epochs/batches with filenames: "epoch-%i-batch-%i-*.pt"')
 parser.add_argument('--bptt', type=int, default=35, help='sequence length')
 parser.add_argument('--ntokens', type=int, default=50001, help='Vocabulary size (default for English)')
 parser.add_argument('--cuda', action='store_true', help='use CUDA')
