08fac04103f75ab3eac928128721680970e5edd2
FAIRNS <yair.lakretz@cea.fr>
2019-10-17 09:21:44 +0200

    interaction plots included

--- Code/behav_experiment/plot_results.py
+++ Code/behav_experiment/plot_results.py
@@ -89,38 +89,84 @@ plt.savefig(os.path.join(path2figures, fn))
 ######################################################
 fig, axes = plt.subplots(1, 1, figsize=(10, 10))
 df_interaction = []
-for i, s_type in enumerate(['objrel', 'objrel_nounpp', 'embedding_mental_SR', 'embedding_mental_LR']):
-    curr_dict = {}
-    if s_type in ['objrel_nounpp', 'embedding_mental_LR']:
-        test_conditions = ['SPS', 'PSP']
-        control_conditions = ['SSS', 'PPP']
-        SR_LR = 'LR'
-    elif s_type in ['objrel', 'embedding_mental_SR']:
-        test_conditions = ['SP', 'PS']
-        control_conditions = ['SS', 'PP']
-        SR_LR = 'SR'
-    if s_type in ['objrel', 'objrel_nounpp']:
-        succ_nested = 'Nested'
-    elif s_type in ['embedding_mental_SR', 'embedding_mental_LR']:
-        succ_nested = 'Succesive'
 
+for subject in df_error_rates.subject.unique():
+    for i, s_type in enumerate(['objrel', 'objrel_nounpp', 'embedding_mental_SR', 'embedding_mental_LR']):
+        curr_dict = {}
+        if s_type == 'objrel':
+            test_conditions = ['SP', 'PS']
+            control_conditions = ['SS', 'PP']
+            SR_LR = 'SR'
+            succ_nested = 'Nested'
+        elif s_type == 'objrel_nounpp':
+            test_conditions = ['SPS', 'PSP']
+            control_conditions = ['SSS', 'PPP']
+            SR_LR = 'LR'
+            succ_nested = 'Nested'
+        elif s_type == 'embedding_mental_SR':
+            test_conditions = ['SP', 'PS']
+            control_conditions = ['SS', 'PP']
+            SR_LR = 'SR'
+            succ_nested = 'Succesive'
+        elif s_type == 'embedding_mental_LR':
+            test_conditions = ['SPS', 'PSP']
+            control_conditions = ['SSS', 'PPP']
+            SR_LR = 'LR'
+            succ_nested = 'Succesive'
 
-    df = df_error_rates.loc[(df_error_rates['sentence_type'] == s_type) & (df_error_rates['trial_type'] == 'Violation') & (df_error_rates['violation_position'].isin(['inner']))]
-    curr_dict['mean_error_rate'] = df.loc[(df['condition'].isin(test_conditions))]['error_rate'].mean()
-    curr_dict['std_error_rate'] = df.loc[(df['condition'].isin(test_conditions))]['error_rate'].std()
-    curr_dict['mean_error_rate_norm'] = df.loc[(df['condition'].isin(test_conditions))]['error_rate'].mean() - df.loc[(df['condition'].isin(control_conditions))]['error_rate'].mean()
-    curr_dict['std_error_rate_norm'] = df.loc[(df['condition'].isin(test_conditions))]['error_rate'].mean() - df.loc[(df['condition'].isin(control_conditions))]['error_rate'].std()
+        df = df_error_rates.loc[(df_error_rates['subject'] == subject) & (df_error_rates['sentence_type'] == s_type) & (df_error_rates['trial_type'] == 'Violation') & (df_error_rates['violation_position'].isin(['inner']))]
+        assert len(df.loc[(df['condition'].isin(test_conditions))]['error_rate']) == 2
+        curr_dict['mean_error_rate'] = df.loc[(df['condition'].isin(test_conditions))]['error_rate'].mean()
+        curr_dict['mean_error_rate_norm'] = df.loc[(df['condition'].isin(test_conditions))]['error_rate'].mean() - df.loc[(df['condition'].isin(control_conditions))]['error_rate'].mean()
 
-    curr_dict['sentence_type'] = s_type
-    curr_dict['SR_LR'] = SR_LR
-    curr_dict['succ_nested'] = succ_nested
-    df_interaction.append(curr_dict)
+        curr_dict['subject'] = subject
+        curr_dict['sentence_type'] = s_type
+        curr_dict['SR_LR'] = SR_LR
+        curr_dict['succ_nested'] = succ_nested
+        df_interaction.append(curr_dict)
 
 df_interaction = pd.DataFrame(df_interaction)
-interaction_plot(df_interaction.SR_LR, df_interaction.succ_nested, df_interaction.error_rate, colors=['red','blue'], markers=['D','^'], ms=10)
 
-fn = 'interaction_inner_error_rate_humans.png'
-plt.savefig(os.path.join(path2figures, fn))
+SR_succesive = df_interaction.loc[(df_interaction['SR_LR']=='SR') & (df_interaction['succ_nested']=='Succesive')]
+LR_succesive = df_interaction.loc[(df_interaction['SR_LR']=='LR') & (df_interaction['succ_nested']=='Succesive')]
+SR_nested = df_interaction.loc[(df_interaction['SR_LR']=='SR') & (df_interaction['succ_nested']=='Nested')]
+LR_nested = df_interaction.loc[(df_interaction['SR_LR']=='LR') & (df_interaction['succ_nested']=='Nested')]
 
-result = ols(formula='error_rate ~ SR_LR + succ_nested + SR_LR * succ_nested', data=df_interaction).fit()
+import numpy as np
+num_subjects = len(df_error_rates.subject.unique())
+def generate_interaction_plot(y_name):
+    fix, ax = plt.subplots(figsize=(20, 10))
+    ax.errorbar([0, 1], [SR_succesive[y_name].mean(), LR_succesive[y_name].mean()], yerr=[SR_succesive[y_name].std()/np.sqrt(num_subjects), LR_succesive[y_name].std()/np.sqrt(num_subjects)], marker='.', ls='--', color='c', label='Succesive', lw=2)
+    ax.errorbar([0, 1], [SR_nested[y_name].mean(), LR_nested[y_name].mean()], yerr=[SR_nested[y_name].std()/np.sqrt(num_subjects), LR_nested[y_name].std()/np.sqrt(num_subjects)], marker='+', ls='-', color='r', label='Nested', lw=2)
+    ax.set_xticks((0,1))
+    ax.set_xticklabels(('Short-range', 'Long-range'), fontsize=26)
+    ax.tick_params(axis='y', which='major', labelsize=14)
+    ax.set_xlim((-0.2, 1.2))
+    ax.set_ylim((0, 1))
+    ax.set_ylabel('Error rate on V2', fontsize=30)
+    ax.axhline(0.5, ls=':', color='k', label='Chance')
+    ax.legend(loc='upper left', bbox_to_anchor=(1, 1), fontsize=20)
+    ax.set_title('Humans', fontsize=30)
+    plt.subplots_adjust(right=0.8)
+
+    fn = 'Interaction_Italian_humans_' + y_name + '.png'
+    plt.savefig(os.path.join(path2figures, fn))
+
+generate_interaction_plot('mean_error_rate')
+generate_interaction_plot('mean_error_rate_norm')
+
+
+################
+### STATS ######
+################
+
+result = ols(formula='mean_error_rate ~ SR_LR + succ_nested + SR_LR * succ_nested', data=df_interaction).fit()
 print(result.summary())
+
+result = ols(formula='mean_error_rate_norm ~ SR_LR + succ_nested + SR_LR * succ_nested', data=df_interaction).fit()
+print(result.summary())
+
+
+# interaction_plot(df_interaction.SR_LR, df_interaction.succ_nested, df_interaction.mean_error_rate, colors=['red','blue'], markers=['D','^'], ms=10)
+# fn = 'interaction_inner_error_rate_humans.png'
+# plt.savefig(os.path.join(path2figures, fn))
